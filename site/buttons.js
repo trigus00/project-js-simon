f09a73a84eb93ce63e17aae537a635518f3c0629
Xavier  Ramirez <xavierramirez@Xaviers-MacBook-Pro.local>
2021-11-11 16:04:20 -0600

    adding final functionality on game and looping, still need lose clause

--- site/buttons.js
+++ site/buttons.js
@@ -9,72 +9,88 @@ function displayListOfButtons() {
 let greenButton = document.querySelector('.simon-button.green');
 greenButton.addEventListener('mousedown', function () {
   //console.log('You clicked the green button');
-  greenButton.classList.add('pressed');
-  // Audio clip 
-  var audio = new Audio("simonSound1.mp3");
-  audio.play();
+  if (canPush) {
+    greenButton.classList.add('pressed');
+    // Audio clip 
+    var audio = new Audio("simonSound1.mp3");
+    audio.play();
 
-  listOfButtons.push("green")
-  pressCounter++;
-  compareList(listOfButtons, pressCounter)
+    listOfButtons.push("green")
+    pressCounter++;
+    compareList(listOfButtons, pressCounter)
+  }
 });
 greenButton.addEventListener('mouseup', function () {
   //console.log("you released the green button");
-  greenButton.classList.remove('pressed');
+  if (canPush) {
+    greenButton.classList.remove('pressed');
+  }
 
   //displayListOfButtons()
 });
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('mousedown', function () {
-  //console.log('You clicked the red button');
-  redButton.classList.add('pressed');
+  if (canPush) {
+    //console.log('You clicked the red button');
+    redButton.classList.add('pressed');
 
-  //Audio clip
-  var audio = new Audio("simonSound1.mp3");
-  audio.play();
+    //Audio clip
+    var audio = new Audio("simonSound1.mp3");
+    audio.play();
 
-  listOfButtons.push("red")
-  pressCounter++;
-  compareList(listOfButtons, pressCounter)
+    listOfButtons.push("red")
+    pressCounter++;
+    compareList(listOfButtons, pressCounter)
+  }
 });
 redButton.addEventListener('mouseup', function () {
   //console.log("you released the red button");
-  redButton.classList.remove('pressed');
+  if (canPush) {
+    redButton.classList.remove('pressed');
+  }
   //displayListOfButtons()
 });
 
 let yellowButton = document.querySelector('.simon-button.yellow');
 yellowButton.addEventListener('mousedown', function () {
-  //console.log('You clicked the yellow button');
-  yellowButton.classList.add('pressed');
-  // Audio clip 
-  var audio = new Audio("simonSound1.mp3");
-  audio.play();
+  if (canPush) {
+    //console.log('You clicked the yellow button');
+    yellowButton.classList.add('pressed');
+    // Audio clip 
+    var audio = new Audio("simonSound1.mp3");
+    audio.play();
 
-  listOfButtons.push("yellow")
-  pressCounter++
-  compareList(listOfButtons, pressCounter)
+    listOfButtons.push("yellow")
+    pressCounter++
+    compareList(listOfButtons, pressCounter)
+  }
 });
 yellowButton.addEventListener('mouseup', function () {
   //console.log("you released the yellow button");
-  yellowButton.classList.remove('pressed');
+  if (canPush) {
+    yellowButton.classList.remove('pressed');
+  }
   //displayListOfButtons()
 });
 
 let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('mousedown', function () {
-  //console.log('You clicked the blue button');
-  // audio file 
-  var audio = new Audio("simonSound1.mp3");
-  audio.play();
-  blueButton.classList.add('pressed');
-  listOfButtons.push("blue")
-  pressCounter++;
-  compareList(listOfButtons, pressCounter)
+  if (canPush) {
+    //console.log('You clicked the blue button');
+    // audio file 
+    var audio = new Audio("simonSound1.mp3");
+    audio.play();
+    blueButton.classList.add('pressed');
+    listOfButtons.push("blue")
+    pressCounter++;
+    compareList(listOfButtons, pressCounter)
+  }
 });
 blueButton.addEventListener('mouseup', function () {
   //console.log("you released the blue button");
-  blueButton.classList.remove('pressed');
+  if (canPush) {
+    blueButton.classList.remove('pressed');
+  }
   //displayListOfButtons()
 });
--- site/simon.js
+++ site/simon.js
@@ -1,5 +1,7 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
+let canPush = true;
+let gameList = []
 
 function createList(list) {
     let randomButton = Math.floor(Math.random() * 4);
@@ -21,6 +23,7 @@ function createList(list) {
 }
 
 async function displayList(list) {
+    canPush = false;
     console.log(list)
     let tempButton = document.querySelector(`.simon-button.${list[0]}`);
     tempButton.classList.add('pressed');
@@ -40,14 +43,20 @@ async function displayList(list) {
                 resolve();
             }, 300 - (list.length * 12.5)))
         }
+        await new Promise((resolve) => setTimeout(function () {
+            tempButton.classList.remove('pressed');
+            resolve();
+        }, 700 - (list.length * 25)));
     }
     else
     {
-        setTimeout(function()
+        await new Promise((resolve) => setTimeout(function()
         {
             tempButton.classList.remove('pressed');
-        }, 500)
+            resolve()
+        }, 500));
     }
+    canPush = true;
 }
 function fakeUnpress(color) {
     let tempButton = document.querySelector(`.simon-button.${color}`);
@@ -63,13 +72,34 @@ function compareList(listOfInputs, pressCount) {
     }
 
     if (matchingLists == true && pressCount == gameList.length)
-        console.log("You won baybeee")
+        continueGame();
     else if (matchingLists == false) {
-        console.log("wow you suck");
+        loseGame();
     }
 }
 
-let gameList = [];
+/*let gameList = [];
 //createList(gameList);
 //createList(gameList);
-displayList(['green', 'blue', 'red', 'yellow', 'green', 'green'])
+displayList(['green', 'blue', 'red', 'yellow', 'green', 'green'])*/
+
+function startGame()
+{
+    //let gameList = [];
+    
+    createList(gameList);
+}
+
+function continueGame()
+{
+    console.log("Hey nice, good round");
+    setTimeout(function () {
+        createList(gameList);
+    }, 2000)
+}
+function loseGame()
+{
+    console.log("Wow you suck. Play again?");
+}
+
+startGame()
