accf6db5aad1e398c993264d3b1c2cd4032fffd2
Xavier  Ramirez <xavierramirez@Xaviers-MacBook-Pro.local>
2021-11-11 11:40:07 -0600

    you can now press buttons!

--- site/buttons.js
+++ site/buttons.js
@@ -1,19 +1,39 @@
 let greenButton = document.querySelector('.simon-button.green');
-greenButton.addEventListener('click', function() {
-  alert('You clicked the green button');
+greenButton.addEventListener('mousedown', function() {
+  //console.log('You clicked the green button');
+  greenButton.classList.add('pressed');
+});
+greenButton.addEventListener('mouseup', function() {
+  //console.log("you released the green button");
+  greenButton.classList.remove('pressed');
 });
 
 let redButton = document.querySelector('.simon-button.red');
-redButton.addEventListener('click', function() {
-  alert('You clicked the red button');
+redButton.addEventListener('mousedown', function() {
+  //console.log('You clicked the red button');
+  redButton.classList.add('pressed');
+});
+redButton.addEventListener('mouseup', function() {
+  //console.log("you released the red button");
+  redButton.classList.remove('pressed');
 });
 
 let yellowButton = document.querySelector('.simon-button.yellow');
-yellowButton.addEventListener('click', function() {
-  alert('You clicked the yellow button');
+yellowButton.addEventListener('mousedown', function() {
+  //console.log('You clicked the yellow button');
+  yellowButton.classList.add('pressed');
+});
+yellowButton.addEventListener('mouseup', function() {
+  //console.log("you released the yellow button");
+  yellowButton.classList.remove('pressed');
 });
 
 let blueButton = document.querySelector('.simon-button.blue');
-blueButton.addEventListener('click', function() {
-  alert('You clicked the blue button');
+blueButton.addEventListener('mousedown', function() {
+  console.log('You clicked the blue button');
+  blueButton.classList.add('pressed');
+});
+blueButton.addEventListener('mouseup', function() {
+  console.log("you released the blue button");
+  blueButton.classList.remove('pressed');
 });
