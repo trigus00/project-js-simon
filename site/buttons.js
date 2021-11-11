558e0c00bf7b964aac320de6d488cb2212c47be0
Xavier  Ramirez <xavierramirez@Xaviers-MacBook-Pro.local>
2021-11-11 12:25:05 -0600

    added some extra button stuff and starting on creating the actual game

--- site/buttons.js
+++ site/buttons.js
@@ -1,39 +1,60 @@
+let listOfButtons = []
+let pressCounter = 0;
+
+function displayListOfButtons()
+{
+  console.log(`You have pressed ${pressCounter} buttons:`)
+  console.log(listOfButtons)
+}
+
 let greenButton = document.querySelector('.simon-button.green');
 greenButton.addEventListener('mousedown', function() {
   //console.log('You clicked the green button');
   greenButton.classList.add('pressed');
+  listOfButtons.push("green")
+  pressCounter++;
 });
 greenButton.addEventListener('mouseup', function() {
   //console.log("you released the green button");
   greenButton.classList.remove('pressed');
+  displayListOfButtons()
 });
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('mousedown', function() {
   //console.log('You clicked the red button');
   redButton.classList.add('pressed');
+  listOfButtons.push("red")
+  pressCounter++;
 });
 redButton.addEventListener('mouseup', function() {
   //console.log("you released the red button");
   redButton.classList.remove('pressed');
+  displayListOfButtons()
 });
 
 let yellowButton = document.querySelector('.simon-button.yellow');
 yellowButton.addEventListener('mousedown', function() {
   //console.log('You clicked the yellow button');
   yellowButton.classList.add('pressed');
+  listOfButtons.push("yellow")
+  pressCounter++
 });
 yellowButton.addEventListener('mouseup', function() {
   //console.log("you released the yellow button");
   yellowButton.classList.remove('pressed');
+  displayListOfButtons()
 });
 
 let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('mousedown', function() {
-  console.log('You clicked the blue button');
+  //console.log('You clicked the blue button');
   blueButton.classList.add('pressed');
+  listOfButtons.push("blue")
+  pressCounter++;
 });
 blueButton.addEventListener('mouseup', function() {
-  console.log("you released the blue button");
+  //console.log("you released the blue button");
   blueButton.classList.remove('pressed');
+  displayListOfButtons()
 });
--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,35 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
+function createList(listToFollow)
+{
+    let randomButton = Math.floor(Math.random() * 4);
+    switch(randomButton)
+    {
+        case 0:
+            listToFollow.push("green");
+            break;
+        case 1:
+            listToFollow.push("red");
+            break;
+        case 2:
+            listToFollow.push("blue");
+            break;
+        case 3:
+            listToFollow.push("yellow");
+            break;
+    }
+    console.log(listToFollow);
+}
+
+function compareList(listOfInputs, pressCount)
+{
+    let matchingLists = true;
+    if(pressCount == gameList.length)
+    {
+        
+    }
+}
+
+let gameList = [];
+createList(gameList);
+createList(gameList);
